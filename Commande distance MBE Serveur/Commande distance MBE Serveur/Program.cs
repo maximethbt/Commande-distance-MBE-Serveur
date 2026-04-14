0994e332d9e7475443d36c507a46e2251255086b
Maxime <maxime.thiebaut07@gmail.com>
2026-04-14 12:53:32 +0200

    ajout initial du serveur

---
+++ Commande distance MBE Serveur/Commande distance MBE Serveur/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Commande_distance_MBE_Serveur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
