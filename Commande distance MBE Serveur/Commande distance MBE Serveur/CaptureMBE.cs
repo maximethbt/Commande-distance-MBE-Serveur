7d45a4a5c361ecb5991e09c4e1de23079ba8dbf3
Maxime <maxime.thiebaut07@gmail.com>
2026-04-17 13:51:47 +0200

    ajout du fichier de classes pour la capture

---
+++ Commande distance MBE Serveur/Commande distance MBE Serveur/CaptureMBE.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Capture_ecran_MBE
+{
+    static class CaptureMBE
+    {
+        private static Rectangle bounds;
+        private static Bitmap screenshot;
+        private static Graphics g;
+
+        public static Bitmap Capture(int ScreenNum)
+        {
+            bounds = Screen.AllScreens[ScreenNum-1].Bounds;
+            screenshot = new Bitmap(bounds.Width, bounds.Height);
+            g = Graphics.FromImage(screenshot);
+            g.CopyFromScreen(bounds.X, bounds.Y, 0, 0, bounds.Size);
+            return new Bitmap(screenshot);
+            g.Dispose();
+        }
+        
+
+    }
+}
